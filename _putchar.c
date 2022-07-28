32c887530c10e9ea7c93274c16360eece911e3eb
Ken-Obieze <Kenneth.Obieze@outlook.com>
2022-07-28 12:13:56 +0000

    added _putchar.c and .gitignore

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
+/** 
+ * _putchar - prints character
+ * @c: character to print
+ * Return: on success 1, on error -1
  */
-int _putchar(char c)
+
+int _putchar(char 0)
 {
 	return (write(1, &c, 1));
 }
