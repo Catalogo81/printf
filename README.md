c2d88b9806f9c1a96f86c4f6edd0044853fa4bcf
Catalogo81 <peacematjato@gmail.com>
2022-08-06 05:58:35 -0700

    README.md, main.c and _printf.c files added

---
+++ README.md
@@ -0,0 +1,3 @@
+#0x11. C - printf
+
+This is a printf Repository that contains all the files for the group assignment.
---
+++ _printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: _printf.c
+ * Auth: Kgotso Matjato
+ * 	 Ejiofor Obieze
+ */
+
+#include "main.h"
+
+/**
+ * _printf: Prints out any type of formatted string
+ * @format: String character
+ *
+ * Return: Characters printed
+ */
+int _printf(const char *format, ...)
+{
+
+}
